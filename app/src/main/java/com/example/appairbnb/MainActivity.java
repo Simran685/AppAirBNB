72bdbb8348e76c2ce23bb7acfd53abb90829350d
simran16696 <simranmuhar@gmail.com>
2017-05-17 15:47:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/appairbnb/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.appairbnb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public void mainclick(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+        public void main2click(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+
